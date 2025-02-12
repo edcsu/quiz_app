9d8cf4d2a176c49979032eaf43116b89527be2dd
Ssewannonda Keith Edwin <skeith696@gmail.com>
2025-02-12 15:32:36 +0300

    more logic for quiz

---
+++ src/components/Quiz.jsx
@@ -0,0 +1,48 @@
+import { useState } from 'react'
+import QUESTIONS from '../questions'
+import quizCompleteImage from '../assets/quiz-complete.png'
+
+function Quiz() {
+    const [userAnswers, setUserAnswers] = useState([])
+    
+    const activeQuestionIndex = userAnswers.length
+    
+    const isQuizComplete = activeQuestionIndex === QUESTIONS.length
+
+    function handleSelectAnswer(selectedAnswer) {
+        setUserAnswers((prevState) => {
+            return [...prevState, selectedAnswer]
+        })
+    }
+
+    if (isQuizComplete) {
+        return (
+            <div id='summary'>
+                <img src={quizCompleteImage} alt="quiz is complete" />
+                <h2>Quiz Completed!</h2>
+            </div>
+        )
+    }
+
+    const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers]
+    shuffledAnswers.sort(() => Math.random() -0.5)
+
+    return (
+        <div id='quiz'>
+            <div id="question">
+                <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+                <ul id="answers">
+                    {shuffledAnswers.map((answer) => (
+                        <li key={answer} className="answer">
+                            <button onClick={() => handleSelectAnswer(answer)}>
+                                {answer}
+                            </button>
+                        </li>
+                    ))}
+                </ul>
+            </div>    
+        </div>
+    )
+}
+
+export default Quiz
